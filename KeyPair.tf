4666484fca8803d74d60f7f947b11c80f39b4d79
AshishSingh1503 <23bec025@nith.ac.in>
2025-06-19 02:53:12 +0530

    first

---
+++ KeyPair.tf
@@ -0,0 +1,5 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dove-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKR6MpXqyrSC04YzDsmwkSaUInNHqpChruwgOOms9+Xa Ashish@Ashish_Singh"
+}
+
